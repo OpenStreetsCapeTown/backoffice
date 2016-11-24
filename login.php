6afc157f69e3bbeed74d6d4d48d484be1da615c0
Paul Hoekman <paul@penguinprotocols.com>
2016-11-24 15:27:41 +0200

    Debug 2

--- login.php
+++ login.php
@@ -61,6 +61,9 @@ if ($_GET['state']) {
   $_SESSION['state'] = $state;
 }
 
+var_dump($_SESSION);
+die(var_dump($_COOKIE));
+
 ?>
 <!doctype html>
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
