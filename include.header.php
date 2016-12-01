40c42ff2e3fced0c61cda3aef0a9d7796fd69c7b
Paul Hoekman <paul@penguinprotocols.com>
2016-12-01 11:10:16 +0200

    Users option 10

--- include.header.php
+++ include.header.php
@@ -65,6 +65,14 @@
                 <li><a href="checklist/template">Checklist</a></li>
               </ul>
             </li>
+            <?php if (OPENID_USERID == 1 || OPENID_USERID == 4 || OPENID_USERID == 2) { ?>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="info/users">Users</a></li>
+              </ul>
+            </li>
+            <?php } ?>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li><a href="cms/logout">Log Out</a></li>
--- users.php
+++ users.php
@@ -1,5 +1,20 @@
 <?php
 require_once 'functions.php';
+
+if (OPENID_USERID != 1 && OPENID_USERID != 4 && OPENID_USERID != 2) {
+  die("No access");
+}
+
+$reactivate = (int)$_GET['reactivate'];
+if ($reactivate) {
+  $db->query("UPDATE openid_users SET status = 1 WHERE id = $reactivate");
+}
+
+$delete = (int)$_GET['delete'];
+if ($delete) {
+  $db->query("UPDATE openid_users SET status = 0 WHERE id = $delete");
+}
+
 $list = $db->query("SELECT * FROM openid_users ORDER BY id");
 ?>
 <!doctype html>
@@ -17,17 +32,27 @@ $list = $db->query("SELECT * FROM openid_users ORDER BY id");
 
 <table class="table table-striped">
     <tr>
+        <th>ID</th>
         <th>Name</th>
         <th>Mail</th>
         <th>Creation Date</th>
         <th>Status</th>
+        <th>Actions</th>
     </tr>
 <?php while ($row = $list->fetch()) { ?>
     <tr class="status-<?php echo $row['status'] ?>">
+        <td><?php echo $row['id'] ?></td>
         <td><?php echo $row['screenname'] ?></td>
         <td><?php echo $row['mail'] ?></td>
         <td><?php echo format_date("M d, Y", $row['creation_date']) ?></td>
         <td><?php echo $row['status'] ? "Active" : "Inactive" ?></td>
+        <td>
+          <?php if ($row['status']) { ?>
+            <a href="users.php?delete=<?php echo $row['id'] ?>" class="btn btn-danger">Deactivate</a>
+          <?php } else { ?>
+            <a href="users.php?reactivate=<?php echo $row['id'] ?>" class="btn btn-success">Reactivate</a>
+          <?php } ?>
+        </td>
     </tr>
 <?php } ?>
 </table>
